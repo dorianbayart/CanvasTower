392064c2aea59332b053c1c5aa58526ecddaa785
Dorian Bayart <dorianbayart@users.noreply.github.com>
2018-10-08 16:43:53 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ var canvasYSize;
 var landscape = true;
 var ctx;
 var fpsGame = 0;
-var fpsGameTab = 5;
+var fpsGameTab = 4;
 var fpsUI = 0;
-var fpsUIDesired = 15;
-var fpsUITab = 10;
-var delayUI = 50;
+var fpsUIDesired = 20;
+var fpsUITab = 4;
+var delayUI = 40;
 var dateGame0, dateGame1;
 var dateUI0, dateUI1;
 var creeps = [];
@@ -19,42 +19,45 @@ var Creep = {
   vx: 1,
   vy: 1,
   speed: 2,
-  radius: 5,
+  radius: 10,
   color: 'blue',
   draw: function(ctx) {
-    ctx.beginPath();
-    ctx.arc(Math.round(this.x), Math.round(this.y), this.radius, 0, Math.PI*2, false);
-    ctx.closePath();
-    ctx.strokeStyle = this.color;
-    ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(Math.round(this.x), Math.round(this.y), Math.round(this.radius), 0, Math.PI*2, false);
+	ctx.closePath();
+	ctx.strokeStyle = this.color;
+	ctx.stroke();
   }
 };
 
 function init(){
   var canvasGame = document.getElementById('game-layer');
   var canvasUI = document.getElementById('ui-layer');
+  var canvasBG = document.getElementById('background-layer');
   if (canvasGame.getContext) {
-    ctxGame = canvasGame.getContext('2d');
-    ctxUI = canvasUI.getContext('2d');
+	ctxGame = canvasGame.getContext('2d');
+	ctxUI = canvasUI.getContext('2d');
+	ctxBG = canvasBG.getContext('2d');
 
-    dateGame0 = performance.now();
-    dateGame1 = dateGame0;
-    dateUI0 = dateGame0;
-    dateUI1 = dateGame0;
+	dateGame0 = performance.now();
+	dateGame1 = dateGame0;
+	dateUI0 = dateGame0;
+	dateUI1 = dateGame0;
 
-    for (var i = 0; i < 100; i++) {
-      var creep = Object.create(Creep);
-      creep.vx = Math.random();
-      creep.vy = Math.random();
-      creeps[i] = creep;
-    }
-	
 	window.addEventListener("resize", resizeGame);
 	window.addEventListener('orientationchange', resizeGame);
 	resizeGame();
+	
+	for (var i = 0; i < 100; i++) {
+	  var creep = Object.create(Creep);
+	  creep.vx = Math.random();
+	  creep.vy = Math.random();
+	  creep.radius = 0.01*canvasXSize;
+	  creeps[i] = creep;
+	}
 
-    window.requestAnimationFrame(drawGame);
-    window.requestAnimationFrame(drawUI);
+	window.requestAnimationFrame(drawGame);
+	window.requestAnimationFrame(drawUI);
   }
 }
 
@@ -71,19 +74,28 @@ function drawGame() {
   ctxGame.lineWidth = 1;
 
   ctxGame.beginPath();
-  ctxGame.arc(150, 150, 100, 0, Math.PI*2, false);
+  ctxGame.arc(0.15*canvasXSize, 0.15*canvasXSize, 0.04*canvasXSize, 0, Math.PI*2, false);
+  ctxGame.closePath();
   ctxGame.strokeStyle = "coral";
   ctxGame.fillStyle = "bisque";
   ctxGame.fill();
   ctxGame.stroke();
+  
+  ctxGame.beginPath();
+  ctxGame.arc(0.85*canvasYSize, 0.85*canvasYSize, 0.04*canvasXSize, 0, Math.PI*2, false);
+  ctxGame.closePath();
+  ctxGame.strokeStyle = "limegreen";
+  ctxGame.fillStyle = "palegreen";
+  ctxGame.fill();
+  ctxGame.stroke();
 
   for (var i = 0; i < creeps.length; i++) {
-    var creep = creeps[i];
-    creep.x += creep.vx * creep.speed /** diffTime/1000.*/;
-    creep.y += creep.vy * creep.speed /** diffTime/1000.*/;
-    if(creep.x > canvasXSize - creep.radius || creep.x < creep.radius) creep.vx = -creep.vx;
-    if(creep.y > canvasYSize - creep.radius || creep.y < creep.radius) creep.vy = -creep.vy;
-    creep.draw(ctxGame);
+	var creep = creeps[i];
+	creep.x += creep.vx * creep.speed /** diffTime/1000.*/;
+	creep.y += creep.vy * creep.speed /** diffTime/1000.*/;
+	if(creep.x > canvasXSize - creep.radius || creep.x < creep.radius) creep.vx = -creep.vx;
+	if(creep.y > canvasYSize - creep.radius || creep.y < creep.radius) creep.vy = -creep.vy;
+	creep.draw(ctxGame);
   }
 
   window.requestAnimationFrame(drawGame);
@@ -94,27 +106,29 @@ function drawUI() {
   dateUI1 = performance.now();
   fpsUI = (fpsUI*(fpsUITab-1) + 1000. / (dateUI1 - dateUI0)) / fpsUITab;
   dateUI0 = dateUI1;
-  if(fpsUI > fpsUIDesired) delayUI ++;
-  else if (fpsUI < fpsUIDesired) delayUI --;
+  if(Math.round(fpsUI) > fpsUIDesired) delayUI ++;
+  else if (Math.round(fpsUI) < fpsUIDesired) delayUI --;
 
   ctxUI.clearRect(0, 0, canvasXSize, canvasYSize);
 
   // Title
-  var text = 'CanvasTower';
+  var copyrightChar = String.fromCharCode(169);
+  var text = 'CanvasTower'+copyrightChar+'Dorian Bayart';
   ctxUI.font = 12+"px Verdana";
   ctxUI.textAlign = "left";
   ctxUI.textBaseline = "top";
   var textPxLength = ctxUI.measureText(text);
   ctxUI.fillStyle = "darkgreen";
-  ctxUI.fillText(text, canvasXSize-8-Math.round(textPxLength.width), 4);
+  ctxUI.fillText(text, canvasXSize-2-Math.round(textPxLength.width), canvasYSize-2-parseInt(ctxUI.font));
 
   // FPS
-  ctxUI.font = 8+"px Verdana";
+  ctxUI.font = 10+"px Verdana";
   ctxUI.textAlign = "left";
   ctxUI.textBaseline = "top";
   ctxUI.fillStyle = "darkgreen";
-  ctxUI.fillText('Game: '+Math.round(fpsGame)+'fps', 2, 0);
-  ctxUI.fillText('UI: '+Math.round(fpsUI)+'fps', 2, 10);
+  ctxUI.fillText(canvasXSize+'x'+canvasYSize+' pixels', 2, 2);
+  ctxUI.fillText('Game: '+Math.round(fpsGame)+'fps', 2, 2+parseInt(ctxUI.font));
+  ctxUI.fillText('UI: '+Math.round(fpsUI)+'fps - Delay: '+delayUI+'ms', 2, 2+2*parseInt(ctxUI.font));
 
   //window.requestAnimationFrame(drawUI);
   setTimeout(drawUI, delayUI);
@@ -123,12 +137,13 @@ function drawUI() {
 function resizeGame() {
 	var canvasGame = document.getElementById('game-layer');
 	var canvasUI = document.getElementById('ui-layer');
+	var canvasBG = document.getElementById('background-layer');
 	
 	// Récupère les dimensions de la fenêtre
-    viewport = {
+	viewport = {
 		width: window.innerWidth,
 		height: window.innerHeight
-    };
+	};
 	var ratioSize = 1.6; // Défini un ratio pour les canvas
 	
 	if(viewport.width > viewport.height) {
@@ -152,11 +167,14 @@ function resizeGame() {
 	}
 	
 	canvasGame.width = canvasXSize;
-    canvasGame.height = canvasYSize;
-    canvasUI.width = canvasXSize;
-    canvasUI.height = canvasYSize;
+	canvasGame.height = canvasYSize;
+	canvasUI.width = canvasXSize;
+	canvasUI.height = canvasYSize;
+	canvasBG.width = canvasXSize;
+	canvasBG.height = canvasYSize;
 	
 	// Centre les canvas
-    canvasGame.style.margin = (viewport.height-canvasYSize)/2 + "px " + (viewport.width-canvasXSize)/2 + "px";
-    canvasUI.style.margin = (viewport.height-canvasYSize)/2 + "px " + (viewport.width-canvasXSize)/2 + "px";
+	canvasGame.style.margin = (viewport.height-canvasYSize)/2 + "px " + (viewport.width-canvasXSize)/2 + "px";
+	canvasUI.style.margin = (viewport.height-canvasYSize)/2 + "px " + (viewport.width-canvasXSize)/2 + "px";
+	canvasBG.style.margin = (viewport.height-canvasYSize)/2 + "px " + (viewport.width-canvasXSize)/2 + "px";
 }
