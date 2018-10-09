1400e95c1bbd4353f3bd06bae1c22ce8e3f3ef05
Dorian Bayart <dorianbayart@users.noreply.github.com>
2018-10-09 23:45:10 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,8 +11,13 @@ var fpsUI = 0;
 var fpsUIDesired = 20;
 var fpsUITab = 4;
 var delayUI = 40;
+var fpsBG = 0;
+var fpsBGDesired = 4;
+var fpsBGTab = 2;
+var delayBG = 200;
 var dateGame0, dateGame1;
 var dateUI0, dateUI1;
+var dateBG0, dateBG1;
 var creeps = [];
 
 var Creep = {
@@ -21,7 +26,7 @@ var Creep = {
   vx: 1,
   vy: 1,
   speed: 2,
-  radius: 10,
+  radius: 2,
   color: 'blue',
   draw: function(ctx) {
 	ctx.beginPath();
@@ -37,30 +42,35 @@ function init(){
   var canvasUI = document.getElementById('ui-layer');
   var canvasBG = document.getElementById('background-layer');
   if (canvasGame.getContext) {
-	ctxGame = canvasGame.getContext('2d');
-	ctxUI = canvasUI.getContext('2d');
-	ctxBG = canvasBG.getContext('2d');
+  	ctxGame = canvasGame.getContext('2d');
+  	ctxUI = canvasUI.getContext('2d');
+  	ctxBG = canvasBG.getContext('2d');
 
-	dateGame0 = performance.now();
-	dateGame1 = dateGame0;
-	dateUI0 = dateGame0;
-	dateUI1 = dateGame0;
+  	dateGame0 = performance.now();
+  	dateGame1 = dateGame0;
+  	dateUI0 = dateGame0;
+  	dateUI1 = dateGame0;
+    dateBG0 = dateGame0;
+  	dateBG1 = dateGame0;
 
-	window.addEventListener("resize", resizeGame);
-  //window.addEventListener("zoom", resizeGame);
-	window.addEventListener('orientationchange', resizeGame);
-	resizeGame();
+  	window.addEventListener("resize", resizeGame);
+    //window.addEventListener("zoom", resizeGame);
+  	window.addEventListener('orientationchange', resizeGame);
+  	resizeGame();
 
-	for (var i = 0; i < 100; i++) {
-	  var creep = Object.create(Creep);
-	  creep.vx = Math.random();
-	  creep.vy = Math.random();
-	  creep.radius = 0.01*gameSize;
-	  creeps[i] = creep;
-	}
+  	for (var i = 0; i < 100; i++) {
+  	  var creep = Object.create(Creep);
+  	  creep.radius = 1/100*gameSize;
+      creep.x = Math.floor(Math.random()*(gameSize-2*creep.radius))+creep.radius;
+  	  creep.y = Math.floor(Math.random()*(gameSize-2*creep.radius))+creep.radius;
+  	  creep.vx = 2*Math.random()-0.5;
+  	  creep.vy = 2*Math.random()-0.5;
+  	  creeps[i] = creep;
+  	}
 
-	window.requestAnimationFrame(drawGame);
-	window.requestAnimationFrame(drawUI);
+  	window.requestAnimationFrame(drawGame);
+  	window.requestAnimationFrame(drawUI);
+    window.requestAnimationFrame(drawBG);
   }
 }
 
@@ -76,33 +86,31 @@ function drawGame() {
 
   ctxGame.lineWidth = 1;
 
-  ctxGame.beginPath();
-  ctxGame.arc(0.1*gameSize, 0.1*gameSize, 0.04*gameSize, 0, Math.PI*2, false);
-  ctxGame.closePath();
   ctxGame.strokeStyle = "coral";
   ctxGame.fillStyle = "bisque";
-  ctxGame.fill();
-  ctxGame.stroke();
-
-  ctxGame.beginPath();
-  ctxGame.arc(0.9*gameSize, 0.9*gameSize, 0.04*gameSize, 0, Math.PI*2, false);
-  ctxGame.closePath();
-  ctxGame.strokeStyle = "limegreen";
-  ctxGame.fillStyle = "palegreen";
-  ctxGame.fill();
-  ctxGame.stroke();
+  for (var i = 0; i < 25; i++) {
+    ctxGame.beginPath();
+    ctxGame.arc(i*0.04*gameSize + 0.04*gameSize/2, 0.4*gameSize, 0.02*gameSize, 0, Math.PI*2, false);
+    ctxGame.closePath();
+    ctxGame.fill();
+    ctxGame.stroke();
+  }
 
   for (var i = 0; i < creeps.length; i++) {
-	var creep = creeps[i];
-	creep.x += creep.vx * creep.speed /** diffTime/1000.*/;
-	creep.y += creep.vy * creep.speed /** diffTime/1000.*/;
-	if(creep.x > gameSize - creep.radius || creep.x < creep.radius) creep.vx = -creep.vx;
-	if(creep.y > gameSize - creep.radius || creep.y < creep.radius) creep.vy = -creep.vy;
-	creep.draw(ctxGame);
+  	var creep = creeps[i];
+  	creep.x += creep.vx * creep.speed /** diffTime/1000.*/;
+  	creep.y += creep.vy * creep.speed /** diffTime/1000.*/;
+  	if(creep.x > gameSize - creep.radius || creep.x < creep.radius) {
+      creep.vx = -creep.vx;
+    }
+  	if(creep.y > gameSize - creep.radius || creep.y < creep.radius) {
+      creep.vy = -creep.vy;
+    }
+
+  	creep.draw(ctxGame);
   }
 
   window.requestAnimationFrame(drawGame);
-
 }
 
 function drawUI() {
@@ -114,19 +122,6 @@ function drawUI() {
 
   ctxUI.clearRect(0, 0, canvasXSize, canvasYSize);
 
-  // Bar UI
-  ctxUI.beginPath();
-  if(landscape) {
-		ctxUI.moveTo(gameSize+1, 0);
-		ctxUI.lineTo(gameSize+1, gameSize);
-  } else {
-		ctxUI.moveTo(0, gameSize+1);
-		ctxUI.lineTo(gameSize, gameSize+1);
-  }
-  ctxUI.closePath();
-  ctxUI.strokeStyle = "lavender";
-  ctxUI.stroke();
-
   // Title
   var copyrightChar = String.fromCharCode(169);
   var text = 'CanvasTower'+copyrightChar+'Dorian Bayart';
@@ -135,19 +130,65 @@ function drawUI() {
   ctxUI.textBaseline = "top";
   var textPxLength = ctxUI.measureText(text);
   ctxUI.fillStyle = "darkgreen";
-  ctxUI.fillText(text, canvasXSize-2-Math.round(textPxLength.width), canvasYSize-2-parseInt(ctxUI.font));
+  ctxUI.fillText(text, canvasXSize-2-Math.round(textPxLength.width), canvasYSize-4-parseInt(ctxUI.font));
 
   // FPS
   ctxUI.font = 10*devicePixelRatio+"px Verdana";
   ctxUI.textAlign = "left";
   ctxUI.textBaseline = "top";
   ctxUI.fillStyle = "darkgreen";
-  ctxUI.fillText(canvasXSize+'x'+canvasYSize+' pixels ('+devicePixelRatio+')', 2, 2);
+  ctxUI.fillText(canvasXSize+'x'+canvasYSize+'px (DPR:'+devicePixelRatio+')', 2, 2);
   ctxUI.fillText('Game: '+Math.round(fpsGame)+'fps', 2, 2+parseInt(ctxUI.font));
   ctxUI.fillText('UI: '+Math.round(fpsUI)+'fps - Delay: '+delayUI+'ms', 2, 2+2*parseInt(ctxUI.font));
+  ctxUI.fillText('BG: '+Math.round(fpsBG)+'fps - Delay: '+delayBG+'ms', 2, 2+3*parseInt(ctxUI.font));
 
   //window.requestAnimationFrame(drawUI);
   setTimeout(drawUI, delayUI);
+}
+
+function drawBG() {
+  dateBG1 = performance.now();
+  fpsBG = (fpsBG*(fpsBGTab-1) + 1000. / (dateBG1 - dateBG0)) / fpsBGTab;
+  dateBG0 = dateBG1;
+  if(Math.round(fpsBG) > fpsBGDesired) delayBG ++;
+  else if (Math.round(fpsBG) < fpsBGDesired) delayBG --;
+
+  ctxBG.clearRect(0, 0, canvasXSize, canvasYSize);
+
+  ctxBG.lineWidth = 1;
+
+  // Bar UI
+  ctxBG.beginPath();
+  if(landscape) {
+		ctxBG.moveTo(gameSize+1, 0);
+		ctxBG.lineTo(gameSize+1, gameSize);
+  } else {
+		ctxBG.moveTo(0, gameSize+1);
+		ctxBG.lineTo(gameSize, gameSize+1);
+  }
+  ctxBG.closePath();
+  ctxBG.strokeStyle = "lavender";
+  ctxBG.stroke();
+
+  // Start zone : Size = 10%
+  ctxBG.beginPath();
+  ctxBG.arc(0.15*gameSize, 0.15*gameSize, 0.05*gameSize, 0, Math.PI*2, false);
+  ctxBG.closePath();
+  ctxBG.strokeStyle = "coral";
+  ctxBG.fillStyle = "bisque";
+  ctxBG.fill();
+  ctxBG.stroke();
+
+  // End zone : Size = 10%
+  ctxBG.beginPath();
+  ctxBG.arc(0.85*gameSize, 0.85*gameSize, 0.05*gameSize, 0, Math.PI*2, false);
+  ctxBG.closePath();
+  ctxBG.strokeStyle = "limegreen";
+  ctxBG.fillStyle = "palegreen";
+  ctxBG.fill();
+  ctxBG.stroke();
+
+  setTimeout(drawBG, delayBG);
 }
 
 function resizeGame() {
